41be1ee2e393b45e4d9afc456af371cd3634a269
serialc <cyrille@gmail.com>
2023-07-03 22:50:58 +0200

    first

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
 
 include 'html/head.html';
-echo <<< END
-<div class="row">
-   <div class="col-6"> 
-ECHO
-    </div>
-   <div class="col-6"> 
-ECHO
-    </div>
-</div>
-END;
+
+$today = new DateTime(date('Y-m-d'));
+$fmon_thismonth = (new DateTime(date('Y-m-d')))->modify('first monday of this month');
+$fmon_nextmonth = (new DateTime(date('Y-m-d')))->modify('first monday of next month');
+
+// testing
+//echo $today->format('Y-m-d') . '<br>';
+//echo $fmon_thismonth->format('Y-m-d') . '<br>';
+//echo $fmon_nextmonth->format('Y-m-d') . '<br>';
+
+if ($today < $fmon_thismonth) {
+    echo $fmon_thismonth->format('Y-m-d') . ' - ';
+} elseif ($today == $fmon_thismonth) {
+    echo 'Today (' . $fmon_thismonth->format('Y-m-d') . ') - ';
+} else {
+    echo $fmon_nextmonth->format('Y-m-d') . ' - ';
+}
+
 include 'html/foot.html';
 
 ?>
