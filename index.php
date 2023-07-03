c527a152424788dea2fc681db87c3ba8f2317ff9
serialc <cyrille@gmail.com>
2023-07-03 21:05:22 +0200

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'html/head.html';
+echo <<< END
+<div class="row">
+   <div class="col-6"> 
+ECHO
+    </div>
+   <div class="col-6"> 
+ECHO
+    </div>
+</div>
+END;
+include 'html/foot.html';
+
+?>
+
