d1b84561d7fe64251726f67ce1594855dfc88754
serialc <cyrille@gmail.com>
2023-07-04 09:27:21 +0200

    improv

--- index.php
+++ index.php
@@ -2,22 +2,36 @@
 
 include 'html/head.html';
 
+// get todays date, this month's nth Monday, next month's nth Monday
 $today = new DateTime(date('Y-m-d'));
-$fmon_thismonth = (new DateTime(date('Y-m-d')))->modify('first monday of this month');
-$fmon_nextmonth = (new DateTime(date('Y-m-d')))->modify('first monday of next month');
+$fmon_thismonth = (new DateTime(date('Y-m-d')))->modify('second monday of this month');
+$fmon_nextmonth = (new DateTime(date('Y-m-d')))->modify('second monday of next month');
 
-// testing
-//echo $today->format('Y-m-d') . '<br>';
-//echo $fmon_thismonth->format('Y-m-d') . '<br>';
-//echo $fmon_nextmonth->format('Y-m-d') . '<br>';
+// determine which Monday is the right next one (this month's or next month's)
+$event_date = null;
+$is_today = false;
 
 if ($today < $fmon_thismonth) {
-    echo $fmon_thismonth->format('Y-m-d') . ' - ';
+    $event_date = $fmon_thismonth;
 } elseif ($today == $fmon_thismonth) {
-    echo 'Today (' . $fmon_thismonth->format('Y-m-d') . ') - ';
+    $is_today = true;
+    $event_date = $fmon_thismonth;
 } else {
-    echo $fmon_nextmonth->format('Y-m-d') . ' - ';
+    $event_date = $fmon_nextmonth;
 }
+
+// No event in August
+// if it's August then get the following month's Monday
+if ($event_date->format('m') == '08') {
+    $event_date->modify('second monday of next month');
+}
+
+// show event date
+// l = day of week name
+// M = month name
+// d = day of month number
+// Y = full year
+echo $event_date->format('l M d, Y') . ' - ';
 
 include 'html/foot.html';
 
