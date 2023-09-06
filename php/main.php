76cc0c05855bc5ddb59024d9517f6e32e6318e22
Cyrille MdC <cyrille.mdc@gmail.com>
2023-09-06 10:48:16 +0200

    deep copy datetime

--- php/main.php
+++ php/main.php
@@ -6,8 +6,18 @@ include '../html/welcome.html';
 
 // get todays date, this month's nth Monday, next month's nth Monday
 $today = new DateTime(date('Y-m-d'));
-$secmon_thismonth = $today->modify('second monday of this month');
-$secmon_nextmonth = $today->modify('second monday of next month');
+$secmon_thismonth = (clone $today)->modify('second monday of this month');
+$secmon_nextmonth = (clone $today)->modify('second monday of next month');
+
+// debug
+/*
+print($today->format('l, M d, Y'));
+print('<br>');
+print($secmon_thismonth->format('l, M d, Y'));
+print('<br>');
+print($secmon_nextmonth->format('l, M d, Y'));
+print('<br>');
+ */
 
 // determine which Monday is the right next one (this month's or next month's)
 $event_date = null;
@@ -25,6 +35,7 @@ if ($today < $secmon_thismonth) {
 // No event in August
 // if it's August then get the following month's Monday
 if ($event_date->format('m') == '08') {
+    // modify the event_date
     $event_date->modify('second monday of next month');
 }
 
